27e40247cdff41b1dc6d7a17e2e83023c5704a0d
ChristianMelendez <christian.p.melendez@gmail.com>
2017-08-10 15:38:23 -0300

    Hora 15:38

--- src/Publico/Persona.java
+++ src/Publico/Persona.java
@@ -9,8 +9,22 @@ package Publico;
  *
  * @author Español
  */
+import javax.swing.JOptionPane;
 public class Persona {
-    //asd
+int altura=10;
+public int largo=30;
+final int ojos =2;
+public static void main (String[]args){
     
+    Persona Juan= new Persona();
+    Persona Pedro=new Persona ();
+    Juan.altura=20;
+    System.out.println("La altura de Juan es "+Juan.altura+"\nSu largo es "+Juan.largo+" y tiene "+Juan.ojos+" ojos");
+    System.out.println("La altura de Pedro es "+Pedro.altura+"\nSu largo es "+Pedro.largo+" y tiene "+Pedro.ojos+" ojos");
+    
+    JOptionPane.showMessageDialog(null,"La altura de Juan es "+Juan.altura+"\nSu largo es "+Juan.largo+" y tiene "+Juan.ojos+" ojos");
+    JOptionPane.showMessageDialog(null,"La altura de Pedro es "+Pedro.altura+"\nSu largo es "+Pedro.largo+" y tiene "+Pedro.ojos+" ojos");
+    JOptionPane.showMessageDialog(null,"Hola");
+}
     
 }
